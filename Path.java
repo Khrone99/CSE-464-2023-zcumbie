f95ac2d792808e556a242d5788f20a1cd1b62d7a
Zakery R Cumbie <99620396+Khrone99@users.noreply.github.com>
2023-11-05 16:50:56 -0700

    Create Path.java
    BFS Algorithm

---
+++ Path.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.traverse.GraphIterator;
+
+public class Path {
+    public static Graph<String, DefaultEdge> GraphSearch(Graph<String, DefaultEdge> initialGraph, String startNode) {
+        // Create a new graph for the search result
+        Graph<String, DefaultEdge> newGraph = new SimpleGraph<>(DefaultEdge.class);
+
+        // Create a BFS iterator starting from the specified node
+        GraphIterator<String, DefaultEdge> iterator = new BreadthFirstIterator<>(initialGraph, startNode);
+
+        // Add the start node to the search graph
+        newGraph.addVertex(startNode);
+
+        // Traverse the graph using BFS and add nodes and edges to the search graph
+        while (iterator.hasNext()) {
+            String currentVertex = iterator.next();
+            for (DefaultEdge edge : initialGraph.edgesOf(currentVertex)) {
+                String sourceVertex = initialGraph.getEdgeSource(edge);
+                String targetVertex = initialGraph.getEdgeTarget(edge);
+
+                // Add vertices and edges to the search graph
+                newGraph.addVertex(sourceVertex);
+                newGraph.addVertex(targetVertex);
+                newGraph.addEdge(sourceVertex, targetVertex);
+            }
+        }
+
+        return newGraph;
+    }
+}
