deb11c81038caf0d91c7f72c37c699bc0dd60fb0
Zakery R Cumbie <99620396+Khrone99@users.noreply.github.com>
2023-11-05 16:05:18 -0700

    Update featureTester.java
    Part 2 project tests

--- featureTester.java
+++ featureTester.java
@@ -6,6 +6,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class featureTester {
 
@@ -14,7 +18,16 @@ public class featureTester {
         boolean testValid = false;
         Main.parseGraph("C:/Users/ldf08/IdeaProjects/CSE464Project/input.dot");
 
-        assertEquals("(A : B), (B : C), (C : A), (A : C), (D : B)", Main.graph.edgeSet());
+        Set<String> set = new LinkedHashSet<>();
+        set.add("(A : B)");
+        set.add("(B : C)");
+        set.add("(C : A)");
+        set.add("(A : C)");
+        set.add("(D : B)");
+
+        Set<String> unmod = Collections.unmodifiableSet(set);
+
+        assertEquals(unmod, Main.graph.edgeSet());
 
         if (Main.graph.containsVertex("A") && Main.graph.containsVertex("B") && Main.graph.containsVertex("C") && Main.graph.containsVertex("D")) {
             testValid = true;
@@ -82,4 +95,83 @@ public class featureTester {
         File dotFile = new File("C:/Users/ldf08/IdeaProjects/CSE464Project/output.dot");
         assertTrue("DOT file should exist", dotFile.exists());
     }
+
+    // Project Part 2 Test Cases
+
+    @Test
+    public void testRemoveNodeExistingNode() {
+        Main.parseGraph("C:/Users/ldf08/IdeaProjects/CSE464Project/input.dot");
+
+        Main.addNode("V");
+        Main.addNode("P");
+        Main.addEdge("V", "P");
+
+        Main.removeNode("V");
+
+        assertFalse(Main.graph.containsVertex("V"));
+        assertTrue(Main.graph.containsVertex("P"));
+        assertFalse(Main.graph.containsEdge("V", "P"));
+    }
+
+    @Test(expected = NodeNotFoundException.class)
+    public void testRemoveNodeNonExistingNode() {
+        Main.parseGraph("C:/Users/ldf08/IdeaProjects/CSE464Project/input.dot");
+
+        Main.removeNode("Z");
+    }
+
+    @Test
+    public void testRemoveNodesExistingNodes() {
+        Main.parseGraph("C:/Users/ldf08/IdeaProjects/CSE464Project/input.dot");
+
+        String[] labels = {"L", "N", "U"};
+        String[] labels2 = {"L", "U"};
+
+        Main.addNodes(labels);
+
+        Main.removeNodes(labels2);
+
+        assertFalse(Main.graph.containsVertex("L"));
+        assertFalse(Main.graph.containsVertex("U"));
+        assertTrue(Main.graph.containsVertex("N"));
+    }
+
+    @Test(expected = NodeNotFoundException.class)
+    public void testRemoveNodesNonExistingNodes() {
+        Main.parseGraph("C:/Users/ldf08/IdeaProjects/CSE464Project/input.dot");
+
+        String[] labels = {"K", "J", "R"};
+
+        Main.removeNodes(labels);
+    }
+
+    @Test
+    public void testRemoveEdgeExistingEdge() {
+        Main.parseGraph("C:/Users/ldf08/IdeaProjects/CSE464Project/input.dot");
+
+        String[] labels = {"X", "Y", "Z"};
+
+        Main.addNodes(labels);
+
+        Main.addEdge("X", "Z");
+        Main.addEdge("Z", "Y");
+        Main.addEdge("Y", "X");
+
+        Main.removeEdge("Z", "Y");
+
+        assertFalse(Main.graph.containsEdge("Z", "Y"));
+        assertTrue(Main.graph.containsEdge("Y", "X"));
+        assertTrue(Main.graph.containsEdge("X", "Z"));
+    }
+
+    @Test(expected = EdgeNotFoundException.class)
+    public void testRemoveEdgeNonExistingEdge() {
+        Main.parseGraph("C:/Users/ldf08/IdeaProjects/CSE464Project/input.dot");
+
+        Main.addNode("M");
+        Main.addNode("O");
+
+        Main.removeEdge("M", "O");
+    }
+
 }
