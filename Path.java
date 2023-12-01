2623be76e8f528459114c00a7b1fdfdd26db18da
Zakery R Cumbie <99620396+Khrone99@users.noreply.github.com>
2023-11-30 19:29:56 -0700

    Delete Path.java

--- Path.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-import org.jgrapht.Graph;
-import org.jgrapht.graph.DefaultEdge;
-
-public class Path {
-    public static Graph<String, DefaultEdge> GraphSearch(Graph<String, DefaultEdge> initialGraph, String startNode, String dstNode, GraphSearchStrategyPattern strategy) {
-        return strategy.search(initialGraph, startNode, dstNode);
-    }
-}
